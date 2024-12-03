5c99fb1e5a22fafafff3c75fb941a137cffb357e
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2024-12-03 18:28:57 +0000

    WIP on DAY2

--- day2/main.py
+++ day2/main.py
@@ -2,10 +2,20 @@
 x1 = 0
 x2 = 0
 
-def validate(x: str) -> int:
+def validate(a: int,b: int, prevIncreasing: bool, i: int) -> bool:
+    distance = b-a
 
+    if distance > 0:
+            isIncreasing = True
+    else:
+        isIncreasing = False
+
+    if not (distance >= -3 and distance <= 3) or distance == 0 or (i != 0 and prevIncreasing != isIncreasing):
+        return False
+    return True
+
+def partOne(x: str) -> int:
     prevIncreasing = False
-    isIncreasing = False
 
     x = x.replace("\n", "")
 
@@ -15,28 +25,16 @@ def validate(x: str) -> int:
         num1 = nums[i]
         num2  = nums[i+1]
 
-        distance = num2 - num1
-
-        if distance > 0:
-            isIncreasing = True
-        else:
-            isIncreasing = False
-
-        if not (distance >= -3 and distance <= 3) or distance == 0 or (i != 0 and prevIncreasing != isIncreasing):
+        if not validate(num1,num2,prevIncreasing,i):
             return 0
         
-        prevIncreasing = isIncreasing
+        prevIncreasing = (num2 - num1) > 0
 
     return 1
 
-def validate2(x: str) -> int:
-
-    fixable = True
-
-    diff = 0
+def partTwo(x: str) -> int:
 
     prevIncreasing = False
-    isIncreasing = False
 
     x = x.replace("\n", "")
 
@@ -46,26 +44,46 @@ def validate2(x: str) -> int:
         num1 = nums[i]
         num2  = nums[i+1]
 
-        distance = num2 - num1
+        isValid = validate(num1,num2,prevIncreasing,i)
 
-        if distance > 0:
-            isIncreasing = True
-        else:
-            isIncreasing = False
+        if not isValid:
+           if i == 0:
+            firstList = ' '.join(list(map(str,nums[1:])))
+            secList = ' '.join(list(map(str,[nums[0]]+nums[2:])))
 
-        if not (distance >= -3 and distance <= 3) or distance == 0 or (i != 0 and prevIncreasing != isIncreasing):
-            if fixable:
-                fixable = False
-            else:
-                return 0
+            return max(partOne(firstList), partOne(secList))
+           elif i+1 != len(nums)-1:
+    
+               newNums =' '.join(list(map(str,nums[:i+1]+nums[i+2:])))
+               return partOne(newNums)
+           else:
+               return partOne(' '.join(list(map(str,nums[:len(nums)-1]))))
         
-        prevIncreasing = isIncreasing
+        prevIncreasing = (num2 - num1) > 0
     return 1
+        
 
 with open("day2/text.txt") as file:
-    for line in file.readlines():
+    numOfCorrect1 = 0
+    numOfCorrect2 = 0
+
+    lines = file.readlines()
+
+    numOfLines = len(lines)
+
+    for line in lines:
         #PART ONE
-        x1+=validate(line)
-        x2+=validate2(line)
-        print(f"{line.replace("\n","")}: {"SAFE" if validate2(line) == 1 else "UNSAFE"}")
+
+        validation1 = partOne(line)
+        validation2 = partTwo(line)
+
+        x1+=validation1
+        x2+=validation2
+
+        numOfCorrect1+=validation1
+        numOfCorrect2+=validation2
+
+        if validation2 == 0:
+            print(line)
     print(f"Part 1: {x1}\nPart 2: {x2}")
+    print(f"NUMBER OF WRONG FOR PT 1: {numOfLines-numOfCorrect1}\nNUMBER OF WRONG FOR PT 2: {numOfLines-numOfCorrect2}")
