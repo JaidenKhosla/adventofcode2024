944c03d11d91db0beb511000939dfeccac5fc402
Jaiden Khosla <jaiden@khosla.io>
2024-12-03 21:49:07 -0600

    day 5 2015

--- 2015/day5/main.py
+++ 2015/day5/main.py
@@ -23,6 +23,7 @@ def validate(x: str) -> bool:
     return hasDupeLetter and vowels >= 3
 
 def validate2(x: str) -> bool:
+    x = x.replace("\n","")
 
     hasPair = False
     pairsMap: dict[str, list[int]] = {}
@@ -41,17 +42,13 @@ def validate2(x: str) -> bool:
 
             combo = currChar+nextChar
 
-            pairsMap[combo] = pairsMap.get(combo,[]) + [i, i+1]
-        
+            if i not in pairsMap.get(combo,[]) and i+1 not in pairsMap.get(combo,[]):
+                pairsMap[combo] = pairsMap.get(combo,[]) + [i, i+1]
+
         for pair in pairsMap.values():
             if len(pair)/2 < 2:
                 continue
-            
-            for index, item in enumerate(pair):
-                if item in pair[:index]:
-                    continue
             hasDoublePair = True
-
     return hasPair and hasDoublePair
 
 with open("2015/day5/text.txt") as file:
@@ -60,8 +57,6 @@ with open("2015/day5/text.txt") as file:
             numberOfNiceStrings+=1
         if validate2(line):
             numberOfNiceStrings2+=1
-        else:
-            print(line)
     print(f"NUMBER OF NICE STRINGS FOR PART ONE: {str(numberOfNiceStrings)}")
     print(f"NUMBER OF NICE STRINGS FOR PART TWO: {str(numberOfNiceStrings2)}")
     file.close()
