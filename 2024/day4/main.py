764d7fdf177aa0e3d503d752517f12d7108aebbe
Jaiden Khosla <jaiden@khosla.io>
2024-12-04 16:37:15 -0600

    day4 DONEEE

--- 2024/day4/main.py
+++ 2024/day4/main.py
@@ -13,32 +13,47 @@ def usedInXMAS(strMap: list[str]) -> int:
             originX = columnIndex
             originY = rowIndex
 
-            while (originX > 0 and originX < len(row)) and (originY > 0 and originY < len(strMap)):
+            while (originX > 0 and originY > 0):
                 originX-=1
                 originY-=1
             
             reversedOriginX = columnIndex
             reversedOriginY = rowIndex
 
-            while (reversedOriginX > 0 and reversedOriginX < len(row)) and (reversedOriginY > 0 and reversedOriginY < len(strMap)):
+            while reversedOriginX < len(rows) -1 and reversedOriginY > 0:
                 reversedOriginX+=1
                 reversedOriginY-=1
-
             diags[(originX,originY)] = diags.get((originX,originY),"")+strMap[rowIndex][columnIndex]
-            reversedDiags[(reversedOriginX,reversedOriginY)] = reversedDiags.get((reversedOriginX, reversedOriginY),"")+strMap[rowIndex][columnIndex]
+            reversedDiags[(reversedOriginX, reversedOriginY)] = reversedDiags.get((reversedOriginX, reversedOriginY),"")+strMap[rowIndex][columnIndex]
 
     for (p,r) in zip(diags.values(), reversedDiags.values()):
         count+=p.count("XMAS") + p.count("SAMX") + r.count("XMAS") + r.count("SAMX")
     cols = " ".join(cols)
     count+= cols.count("XMAS") + cols.count("SAMX")
 
-    print(cols)
-    print(rows)
-    print(diags)
-    print(reversedDiags)
-
     return count
+
+def usedInXofMAS(strMap: list[str]) -> int:
+
+    count = 0
+
+    for rowIndex, row in enumerate(strMap):
+        for columnIndex, column in enumerate(row):
+            character = column
+            if character != "A" or rowIndex-1 < 0 or rowIndex+1 > len(strMap)-1 or columnIndex-1 < 0 or columnIndex+1 > len(row)-1:
+                continue
+            
+            corners = "".join([strMap[rowIndex-1][columnIndex-1], strMap[rowIndex-1][columnIndex+1],strMap[rowIndex+1][columnIndex-1], strMap[rowIndex+1][columnIndex+1]])
+            if corners in ["MSMS","SMSM","MMSS","SSMM"]:
+                count+=1
+    return count
+
+
 with open("2024/day4/text.txt") as file:
     splittedFile = file.read().split("\n")
     partOne = usedInXMAS(splittedFile)
-    print(partOne)
+    print(f"PARTONE: {partOne}")
+    partTwo = usedInXofMAS(splittedFile)
+    print(f"PARTTWO: {partTwo}")
+
+#spent two hours trying to figure out what was wrong with the my left diagonal for usedInXmas. Turns out it was a single boolean torturing me.
