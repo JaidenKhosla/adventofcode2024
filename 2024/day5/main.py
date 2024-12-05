8b8e4d7cca0b631300abf7e80d915fe6e2fd6d9b
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2024-12-05 16:17:53 +0000

    finished day5

--- 2024/day5/main.py
+++ 2024/day5/main.py
@@ -19,36 +19,36 @@ class Queue:
 
         return nums[len(nums)//2]
     
+    def check(self,queueList: list[int], nums: list[int],num: int):
+        beforeNums = list(filter(lambda x: x in nums, self.rules.get(num,[])))
+        if beforeNums:
+             for beforeNum in beforeNums:
+                if beforeNum not in queueList:
+                    # print(f"{num} can't go before{list(beforeNums)}")
+                    return False
+        return True
+
     def fixInstruction(self, instruction: str) -> int:
         if self.validateInstruction(instruction) > 0:
             return 0
         
         queueList = []
-
+        needToBeResolved = []
         nums = list(map(int, instruction.split(",")))
-        
-        offset = 0
 
-        while len(nums) > 0:
-            num = nums[0+offset]
+        while len(nums) > 0 or len(needToBeResolved) > 0:
+            if len(needToBeResolved) > 0: 
+                if self.check(queueList,nums+needToBeResolved, needToBeResolved[0]):
+                    queueList.append(needToBeResolved.pop(0))
+                else:
+                    needToBeResolved.append(needToBeResolved.pop(0))
+            if  len(nums) > 0:
+                if self.check(queueList, nums+needToBeResolved, nums[0]):
+                    queueList.append(nums.pop(0))
+                else:
+                    needToBeResolved.append(nums.pop(0))
 
-            canBePrinted = True
 
-            beforeNums = filter(lambda x: x in nums, self.rules.get(num,[]))
-
-            for beforeNum in beforeNums:
-                if beforeNum not in queueList:
-                    canBePrinted = False
-                    break
-            if not canBePrinted:
-                nums.pop(0)
-                nums.insert(1,num)
-                continue
-            offset = 0
-
-            queueList.append(num)
-            nums.pop(0+offset)
-        print(queueList)
         return queueList[len(queueList)//2]
 
 with open("2024/day5/test.txt") as file:
@@ -58,12 +58,14 @@ with open("2024/day5/test.txt") as file:
     lines = file.read().split("\n")
 
     partOne = 0
+    partTwo = 0
 
     for line in lines:
         if "," in line:
             partOne += queue.validateInstruction(line)
-            print(queue.fixInstruction(line))
+            partTwo += queue.fixInstruction(line)
         elif "|" in line:
             queue.validateRule(line)
     print(f"Part One: {partOne}")
+    print(f"Part Two: {partTwo}")
     file.close()
